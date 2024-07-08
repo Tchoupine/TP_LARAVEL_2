bd548245bab4056b07e5fa4391389d52b7e04533
rilukafu <fatouhassaneyasmine@gmail.com>
2024-07-08 01:15:02 +0100

    finaly

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,22 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\Freelancer;
 use Illuminate\Http\Request;
-use App\Models\Freelancer;
 
 class PagesController extends Controller
 {
-    public function index()
-    {
+    public function index(){
+        
         return view('index');
     }
 
-    public function table(){
-        
-        return view('crud.table');
+    public function table()
+    {
+        $freelancers = Freelancer::all();
+        $freelancers = Freelancer::paginate(10);
+        return view('crud.table', compact('freelancers'));
     }
 
-    public function details(){
-        
-        return view('details.index');
+    public function details($id)
+    {
+        $freelancer = Freelancer::find($id);
+        return view('details', compact('freelancer'));
     }
 }
